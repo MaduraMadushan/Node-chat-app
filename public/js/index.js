cfd0127ed0c405c5229dc60d319acb16c289316c
MaduraMadushan <39506813+MaduraMadushan@users.noreply.github.com>
2019-01-08 14:32:36 +0530

    Format timestamps using momentjs

--- public/js/index.js
+++ public/js/index.js
@@ -8,17 +8,18 @@ socket.on('disconnect', function(){
 });
 
 socket.on('newMessage', function(message){
-    console.log('NewMessage', message);
+    var formattedTime = moment(message.createdAt).format('h:mm a')
     var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
     jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message){
+    var formattedTime = moment(message.createdAt).format('h:mm a')
     var li = jQuery('<li></li>');
     var a = jQuery('<a target="_blank">My current location</a>');
-    li.text(`${message.from}: `);
+    li.text(`${message.from}: ${formattedTime}`);
     a.attr('href', message.url);
     li.append(a);
     jQuery('#messages').append(li);
@@ -32,7 +33,7 @@ jQuery('#message-form').on('submit', function(e) {
         from: 'User',
         text: messageTextbox.val()
     }, function() {
-        messageTextbox.val('')
+        messageTextbox.val('');
     });
 });
 
@@ -51,7 +52,7 @@ locationButton.on('click', function(){
             longitude: position.coords.longitude
         });
     }, function(){
-        locationButton.removeAttr('disabled');
-        alert('Unable to fetch location.').text('Send location');;
+        locationButton.removeAttr('disabled').text('Send location');
+        alert('Unable to fetch location.')
     })
 });
